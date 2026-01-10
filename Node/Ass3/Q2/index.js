837f6821c19c4cb6b2d0e7f3a339ea0948120f6c
manpoordivyasree26-glitch <manpoordivyasree26@gmail.com>
2026-01-10 23:21:59 +0530

    Ass3

--- Node/Ass3/Q2/index.js
+++ Node/Ass3/Q2/index.js
@@ -3,25 +3,32 @@ import boxen from "boxen";
 const title = "Hurray!!!";
 const message = "I am using my first external module!";
 
-// 1. Classic (default)
-const classicBox = boxen(`${title}\n\n${message}`, {
-  padding: 1,
-  margin: 1
-});
-console.log(classicBox);
+// 1. Classic Style (Default Single Line)
+console.log(
+  boxen(message, {
+    title,
+    padding: 1,
+    margin: 1,
+    borderStyle: "single"
+  })
+);
 
-// 2. SingleDouble style
-const singleDoubleBox = boxen(`${title}\n\n${message}`, {
-  padding: 1,
-  margin: 1,
-  borderStyle: "singleDouble"
-});
-console.log(singleDoubleBox);
+// 2. SingleDouble Style
+console.log(
+  boxen(message, {
+    title,
+    padding: 1,
+    margin: 1,
+    borderStyle: "singleDouble"
+  })
+);
 
-// 3. Round style
-const roundBox = boxen(`${title}\n\n${message}`, {
-  padding: 1,
-  margin: 1,
-  borderStyle: "round"
-});
-console.log(roundBox);
+// 3. Round Style
+console.log(
+  boxen(message, {
+    title,
+    padding: 1,
+    margin: 1,
+    borderStyle: "round"
+  })
+);
